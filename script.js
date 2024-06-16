de6325b7aafb2a1514fa696e623c30055072a2d3
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-06-16 22:15:53 +0300

    looks dope

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-feather.replace()
-//https://twitter.com/One_div
+document.addEventListener('DOMContentLoaded', (event) => {
+    feather.replace();
 
+
+    const imageItems = document.querySelectorAll('.image-item');
+
+    imageItems.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            const img = item.querySelector('img');
+            const details = item.querySelector('.image-details');
+            img.style.transform = 'translateY(-10px)';
+            details.style.transform = 'translateY(0)';
+            details.style.opacity = '1';
+        });
+
+        item.addEventListener('mouseout', () => {
+            const img = item.querySelector('img');
+            const details = item.querySelector('.image-details');
+            img.style.transform = 'translateY(0)';
+            details.style.transform = 'translateY(100%)';
+            details.style.opacity = '0';
+        });
+    });
+});
